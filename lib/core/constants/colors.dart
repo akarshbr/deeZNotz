c2ff8f1bd71711ecef65993df5e5072cc90cc975
akarshbr <akarshbrajeev@gmail.com>
2024-03-13 21:37:14 +0530

    color constants set

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class ColorConstants{
+  static const Color bgColorLight =  Color(0xfff9fbfc);
+  static const Color bgColorDark =  Color(0xff19376d);
+  static const Color cardBGColorLight = Color(0xffa0dbdb);
+  static const Color cardBGColorDark = Color(0xff56a7a7);
+  static const Color textColorLight = Color(0xff000000);
+  static const Color textColorDark = Color(0xffffffff);
+}

bc9aa33d96f02025fb9a50b6bfc4b0f095739b07
akarshbr <akarshbrajeev@gmail.com>
2024-03-14 22:19:03 +0530

    color constants edited

--- lib/core/constants/style.dart
+++ lib/core/constants/style.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 class StyleVariables{
+
+  static const TextStyle logoStyleLight =TextStyle(fontSize: 40, fontWeight: FontWeight.bold);
+  static const TextStyle cardTitleTextStyleLight =TextStyle(fontWeight: FontWeight.bold,fontSize: 30);
+  static const TextStyle cardDescTextStyleLight =TextStyle();
+
   static SliverToBoxAdapter sliverSizedBox(double height) {
     return SliverToBoxAdapter(
       child: SizedBox(height: height),
